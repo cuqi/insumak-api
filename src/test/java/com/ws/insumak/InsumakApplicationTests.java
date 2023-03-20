4430f9795d171afa22a3422272cd129fefe0ccb4
Vladimir Krstic <vladimir.krstic@insumak.mk>
2023-03-20 10:01:30 +0100

    start

---
+++ src/test/java/com/ws/insumak/InsumakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.insumak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsumakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
